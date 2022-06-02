0d17a8230328c8458aba2f48a0b3deb7d36b7d73
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2022-06-02 17:51:01 +0900

    220602

--- movie_1_pr/app/src/main/java/com/example/movie_1_pr/SplashActivity.java
+++ movie_1_pr/app/src/main/java/com/example/movie_1_pr/SplashActivity.java
@@ -3,6 +3,8 @@ package com.example.movie_1_pr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        TextView textView = findViewById(R.id.)
     }
 }
